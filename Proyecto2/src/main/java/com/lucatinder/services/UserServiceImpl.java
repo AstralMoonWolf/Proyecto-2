e43827be76e558149bd5ae8a46d37f769eddaffc
Edmund Home <edmundhome@hotmail.com>
2019-06-27 16:39:52 +0200

    html arreglado

--- Proyecto2/src/main/java/com/lucatinder/services/UserServiceImpl.java
+++ Proyecto2/src/main/java/com/lucatinder/services/UserServiceImpl.java
@@ -1,5 +1,10 @@
 package com.lucatinder.services;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.lucatinder.dao.IUserRepository;
 import com.lucatinder.model.User;
 /**
@@ -9,13 +14,19 @@ import com.lucatinder.model.User;
  * Esta clase se utiliza para llamar a métodos del repositorio por defecto del usuario
  *
  */
+
+@Service
+@Transactional
 public class UserServiceImpl implements IUserService {
 
+	@Autowired
 	private IUserRepository userDAO;
 	
 	/**
 	 * Este método añade un bean de usuario a la bbdd
 	 */
+	
+	@Override
 	public void add(User user) {
 		userDAO.save(user);
 	}
