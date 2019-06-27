8ca2009666c7b83a86dc6347428e1520215e327d
Edmund Home <edmundhome@hotmail.com>
2019-06-27 17:08:42 +0200

    lalala

--- Proyecto2/src/main/java/com/lucatinder/dao/IUserRepositoryCustom.java
+++ Proyecto2/src/main/java/com/lucatinder/dao/IUserRepositoryCustom.java
@@ -1,5 +1,17 @@
 package com.lucatinder.dao;
 
+import java.util.List;
+import com.lucatinder.model.User;
+
+/**
+ * 
+ * @author Andrea
+ * @version 1
+ * Esta clase lista los nombres de los usuarios
+ */
+
 public interface IUserRepositoryCustom {
+	
+	List<User> getFirstNamesLike(String username);
 
 }
--- Proyecto2/src/main/java/com/lucatinder/dao/UserRepositoryImpl.java
+++ Proyecto2/src/main/java/com/lucatinder/dao/UserRepositoryImpl.java
@@ -10,9 +10,25 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import java.util.List;
 
+/**
+ * 
+ * @author Andrea
+ * @version 1
+ * Esta clase recoje los datos (nombres de usuarios) de la BD
+ */
+
 @Repository
 public class UserRepositoryImpl implements IUserRepositoryCustom{
 
+	@PersistenceContext
+	EntityManager entityManager;
 	
+	@Override
+	public List<User> getFirstNamesLike(String username) {
+		Query query = entityManager.createNativeQuery("SELECT em.* FROM spring_data_jpa_example.username as em " +
+                "WHERE em.username LIKE ?", User.class);
+        query.setParameter(1, username + "%");
+        return query.getResultList();
+	}
 	
 }
