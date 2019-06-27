483f7c233c908d275aab471d3a76fac22d6831bf
Edmund Home <edmundhome@hotmail.com>
2019-06-27 15:11:47 +0200

    zcxvxvbxcb

--- Proyecto2/src/main/java/com/lucatinder/dao/IUserRepository.java
+++ Proyecto2/src/main/java/com/lucatinder/dao/IUserRepository.java
@@ -1,9 +1,18 @@
 package com.lucatinder.dao;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.lucatinder.model.User;
+
+/**
+ * 
+ * @author Raul
+ * @version 1
+ * Esta clase tiene la implementacion JPA
+ */
+
 @Repository
 public interface IUserRepository extends JpaRepository<User,Integer>,IUserRepositoryCustom{
 
--- Proyecto2/src/main/java/com/lucatinder/services/IUserService.java
+++ Proyecto2/src/main/java/com/lucatinder/services/IUserService.java
@@ -2,8 +2,20 @@ package com.lucatinder.services;
 
 import com.lucatinder.model.User;
 
+/**
+ * 
+ * @author Raul
+ * @version 1
+ * Esta clase tiene el metodo añadir usuario
+ *
+ */
+
 public interface IUserService {
 
+	/**
+	 * 
+	 * Este metodo añade usuarios
+	 */
 	public void add(User user);
 	
 }
