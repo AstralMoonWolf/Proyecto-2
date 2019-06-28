00bf5d4d3f72dd99f81829be85a5f71798c7f1a2
Edmund Home <edmundhome@hotmail.com>
2019-06-28 18:51:33 +0200

    ola k ase
    ola k ase, cuentame lo k ase

--- Proyecto2/src/main/java/com/lucatinder/controller/HomeController.java
+++ Proyecto2/src/main/java/com/lucatinder/controller/HomeController.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -26,12 +28,13 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	@RequestMapping("/")
+	@GetMapping("/profile")
 	public String handleRequest(ModelMap model) throws Exception {
-		logger.info("-- en Listado");
+		logger.info("-- en Index");
 		model.addAttribute("userList", userService.list());
-		return "index";
+		return "profile";
 	}
+	
 	
 	@PostMapping("/new")
 	public String newUser(ModelMap model) throws Exception  {
