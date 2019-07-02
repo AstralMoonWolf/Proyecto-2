41c51945baa0f97521413378001420391b99c812
Edmund Home <edmundhome@hotmail.com>
2019-07-02 19:45:23 +0200

    xvmffjfd

--- Proyecto2/src/main/java/com/lucatinder/controller/ControladorServicioRest.java
+++ Proyecto2/src/main/java/com/lucatinder/controller/ControladorServicioRest.java
@@ -1,20 +1,12 @@
 package com.lucatinder.controller;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.lucatinder.model.User;
 import com.lucatinder.services.UserService;
 
@@ -24,10 +16,8 @@ public class ControladorServicioRest {
 
 	@Autowired
 	private UserService userService;
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	//Devuelve el usuario de la id que le hemos dado
-	@PostMapping("/profile/{id}")
+
+	@GetMapping("/profile/{id}")
 	public User showUser(@PathVariable int id) {
 		return this.userService.findById(id);
 	}
@@ -42,11 +32,6 @@ public class ControladorServicioRest {
 	public void like(@RequestParam("id1") int id1, @RequestParam("id2") int id2) {
 		//logger.info("-------RestController dislike");
 		userService.like(id1, id2);
-	
-	//método que devuelve la lista de usuarios
-	@GetMapping("/listausuarios")
-	List<User> listausuarios(){
-		return this.userService.list();
 	}
 	
 }
