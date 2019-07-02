46dfd04045dd2b3589a92b93cc9ce2d940b93dad
Edmund Home <edmundhome@hotmail.com>
2019-07-02 19:08:21 +0200

    fff

--- Proyecto2/src/main/java/com/lucatinder/controller/ControladorServicioRest.java
+++ Proyecto2/src/main/java/com/lucatinder/controller/ControladorServicioRest.java
@@ -1,12 +1,20 @@
 package com.lucatinder.controller;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.lucatinder.model.User;
 import com.lucatinder.services.UserService;
 
@@ -16,20 +24,20 @@ public class ControladorServicioRest {
 
 	@Autowired
 	private UserService userService;
-
-	@GetMapping("/profile/{id}")
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	//Devuelve el usuario de la id que le hemos dado
+	@PostMapping("/profile/{id}")
 	public User showUser(@PathVariable int id) {
 		return this.userService.findById(id);
 	}
 	
-	/*@GetMapping("/dislike")
-	public String dislike(@RequestParam("id1") int id1, @RequestParam("id2") int id2, ModelMap model) {
-		logger.info("-------UsuarioController dislike");
-		userService.dislike(id1, id2);
-		model.addAttribute("usuariologin", userService.findById(id1));
-		model.addAttribute("userList", userService.list());
-		return "listprofilesnuevo";
-	}*/
+	
+	//método que devuelve la lista de usuarios
+	@GetMapping("/listausuarios")
+	List<User> listausuarios(){
+		return this.userService.list();
+	}
 	
 
 }
