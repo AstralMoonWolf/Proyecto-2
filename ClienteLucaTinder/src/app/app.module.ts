dd88b186543dc0b24f7d6eec7e717b8bd0abffd8
Edmund Home <edmundhome@hotmail.com>
2019-07-03 11:39:51 +0200

    hfghfghf

--- ClienteLucaTinder/src/app/app.module.ts
+++ ClienteLucaTinder/src/app/app.module.ts
@@ -5,8 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { ListaUsuariosComponent } from './components/lista-usuarios/lista-usuarios.component';
-import { LikeComponent } from './components/like/like.component';
-import { DislikeComponent } from './components/dislike/dislike.component';
+
 import { NewComponent } from './components/new/new.component';
 
 @NgModule({
@@ -14,8 +13,6 @@ import { NewComponent } from './components/new/new.component';
     AppComponent,
     ProfileComponent,
     ListaUsuariosComponent,
-    LikeComponent,
-    DislikeComponent,
     NewComponent
   ],
   imports: [
