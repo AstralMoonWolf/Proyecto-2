7fda3e07ec787b2a4d4be385f967fc96189b86a4
Edmund Home <edmundhome@hotmail.com>
2019-07-04 16:49:33 +0200

    FINAL!

--- ClienteLucaTinder/src/app/components/login/login.component.ts
+++ ClienteLucaTinder/src/app/components/login/login.component.ts
@@ -4,7 +4,6 @@ import { Router } from '@angular/router';
 import { User } from '../../models/user';
 import { UserService } from '../../services/user.service';
 
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -15,7 +14,7 @@ export class LoginComponent implements OnInit {
   user: User;
   constructor(private router: Router, private userService: UserService) { }
 
-  public login(id: number){
+  /*public login(id: number){
     console.log(id)
   
     this.userService.getUser(id).subscribe((user: any) => {
@@ -29,10 +28,31 @@ export class LoginComponent implements OnInit {
       }
     });
   
-  }
-
+  }*/
 
   ngOnInit() {
   }
+
+login(id: number, event: Event) {
+  event.preventDefault(); // Avoid default action for the submit button of the login form
+  // Calls service to login user to the api rest
+  this.userService.getUser(id).subscribe(
+
+    res => {
+      let p: User = {id: id, nombre:null, genero:null, edad:null, poblacion:null, idintereses:null, descripcion:null};
+      this.userService.setPerfilLoggedIn(p);
+    },
+    error => {
+      console.error(error);
+
+    },
+    () => this.navigate()
+  );
+  }
+  navigate() {
+    this.router.navigateByUrl('/profiles');
+  }
+
+
 
 }
--- ClienteLucaTinder/src/app/components/profile/profile.component.ts
+++ ClienteLucaTinder/src/app/components/profile/profile.component.ts
@@ -8,7 +8,8 @@ import { UserService } from '../../services/user.service';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+  providers: [UserService]
 })
 export class ProfileComponent implements OnInit {
 
@@ -20,11 +21,39 @@ export class ProfileComponent implements OnInit {
 
   ngOnInit()
   {
-    this.userService.getUser(this.user.id)
+    //this.user = this.userService.getPerfilLoggedIn();
+    //console.log(this.userService.getPerfilLoggedIn());
+    /*this.userService.getUser(this.userService.getPerfilLoggedIn().id)
       .subscribe( data => {
-        this.user = this.user;
-      })
+        this.user = this.userService.getUser(this.userService.getPerfilLoggedIn().id);
+      })*/
     //this.userService.getUser(this.user.idusuario)
+
+    this.userService.getUser(this.userService.getPerfilLoggedIn().id).subscribe((user: any) => {
+      if (user) {
+        this.user = user;
+        this.userService.setPerfilLoggedIn(user);
+        console.log(user);
+      } else {
+        console.log(`Usuario no encontrado`);
+        //this.gotoList();
+      }
+    });
+
   }
+
+/*
+user: User;
+constructor(private router: Router, private userService: UserService ) { 
+
+}
+
+ngOnInit() 
+{
+  this.user = this.userService.getPerfilLoggedIn();
+  console.log(this.user);
+
+
+}*/
 
 }
